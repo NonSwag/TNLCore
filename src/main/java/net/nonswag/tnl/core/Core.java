cbfd287497dd4de567d8adc6238d3fd85a3f2550
NonSwag <mrminecraft00@gmail.com#>
2022-03-19 17:49:35 +0100

    added Core#getFolder

--- src/main/java/net/nonswag/tnl/core/Core.java
+++ src/main/java/net/nonswag/tnl/core/Core.java
@@ -1,11 +1,17 @@
 package net.nonswag.tnl.core;
 
+import lombok.Getter;
 import net.nonswag.tnl.core.api.file.helper.FileHelper;
 import net.nonswag.tnl.core.api.message.Message;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 
 public class Core {
+
+    @Getter
+    @Nonnull
+    private static final File folder = new File("Core");
 
     private Core() {
     }
