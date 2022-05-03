7f93a0bb98e65f2a1b034816b51c69d8ee8355ca
NonSwag <mrminecraft00@gmail.com#>
2022-05-03 16:43:12 +0200

    added return statements to methods in LinuxUtil

--- src/main/java/net/nonswag/tnl/core/utils/LinuxUtil.java
+++ src/main/java/net/nonswag/tnl/core/utils/LinuxUtil.java
@@ -16,44 +16,44 @@ public final class LinuxUtil {
     private LinuxUtil() {
     }
 
-    public static void runShellCommandUnmodified(@Nonnull String command) throws IOException, InterruptedException {
-        runShellCommandUnmodified(command, Logger.debug);
+    public static int runShellCommandUnmodified(@Nonnull String command) throws IOException, InterruptedException {
+        return runShellCommandUnmodified(command, Logger.debug);
     }
 
-    public static void runShellCommandUnmodified(@Nonnull String command, @Nonnull Logger logger) throws IOException, InterruptedException {
-        runShellCommandUnmodified(command, null, logger);
+    public static int runShellCommandUnmodified(@Nonnull String command, @Nonnull Logger logger) throws IOException, InterruptedException {
+        return runShellCommandUnmodified(command, null, logger);
     }
 
-    public static void runShellCommandUnmodified(@Nonnull String command, @Nonnull File directory) throws IOException, InterruptedException {
-        runShellCommandUnmodified(command, directory, Logger.debug);
+    public static int runShellCommandUnmodified(@Nonnull String command, @Nonnull File directory) throws IOException, InterruptedException {
+        return runShellCommandUnmodified(command, directory, Logger.debug);
     }
 
-    public static void runShellCommandUnmodified(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) throws IOException, InterruptedException {
-        runShellCommand(new String[]{command}, directory, logger);
+    public static int runShellCommandUnmodified(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) throws IOException, InterruptedException {
+        return runShellCommand(new String[]{command}, directory, logger);
     }
 
-    public static void runShellCommand(@Nonnull String command) throws IOException, InterruptedException {
-        runShellCommand(command, Logger.debug);
+    public static int runShellCommand(@Nonnull String command) throws IOException, InterruptedException {
+        return runShellCommand(command, Logger.debug);
     }
 
-    public static void runShellCommand(@Nonnull String command, @Nonnull Logger logger) throws IOException, InterruptedException {
-        runShellCommand(command, null, logger);
+    public static int runShellCommand(@Nonnull String command, @Nonnull Logger logger) throws IOException, InterruptedException {
+        return runShellCommand(command, null, logger);
     }
 
-    public static void runShellCommand(@Nonnull String command, @Nonnull File directory) throws IOException, InterruptedException {
-        runShellCommand(command, directory, Logger.debug);
+    public static int runShellCommand(@Nonnull String command, @Nonnull File directory) throws IOException, InterruptedException {
+        return runShellCommand(command, directory, Logger.debug);
     }
 
-    public static void runShellCommand(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) throws IOException, InterruptedException {
-        runShellCommand(command.split(" "), directory, logger);
+    public static int runShellCommand(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) throws IOException, InterruptedException {
+        return runShellCommand(command.split(" "), directory, logger);
     }
 
-    private static void runShellCommand(@Nonnull String[] command) throws IOException, InterruptedException {
-        runShellCommand(command, null, Logger.debug);
+    private static int runShellCommand(@Nonnull String[] command) throws IOException, InterruptedException {
+        return runShellCommand(command, null, Logger.debug);
     }
 
-    private static void runShellCommand(@Nonnull String[] command, @Nullable File directory, @Nonnull Logger logger) throws IOException, InterruptedException {
-        if (command.length == 0) return;
+    private static int runShellCommand(@Nonnull String[] command, @Nullable File directory, @Nonnull Logger logger) throws IOException, InterruptedException {
+        if (command.length == 0) return -1;
         Process process;
         if (directory == null) {
             if (command.length == 1) process = Runtime.getRuntime().exec(command[0]);
@@ -67,69 +67,73 @@ public final class LinuxUtil {
         while ((string = br.readLine()) != null) logger.println(string);
         process.waitFor();
         logger.println("Finished program with exit code: " + process.exitValue());
+        return process.exitValue();
     }
 
-    public static void runShellScript(@Nonnull ShellFile script) throws IOException, InterruptedException {
-        runShellScript(script, Logger.debug);
+    public static int runShellScript(@Nonnull ShellFile script) throws IOException, InterruptedException {
+        return runShellScript(script, Logger.debug);
     }
 
-    public static void runShellScript(@Nonnull ShellFile script, @Nonnull Logger logger) throws IOException, InterruptedException {
+    public static int runShellScript(@Nonnull ShellFile script, @Nonnull Logger logger) throws IOException, InterruptedException {
         String[] content = script.getContent();
         for (int i = 0; i < content.length; i++) content[i] = Message.format(content[i], script.getPlaceholders());
-        for (String line : content) runShellCommand(line, script.getFile().getAbsoluteFile().getParentFile(), logger);
+        return runShellCommand(String.join("\n", content), script.getFile().getAbsoluteFile().getParentFile(), logger);
     }
 
     public static class Suppressed {
 
-        public static void runShellCommandUnmodified(@Nonnull String command) {
-            runShellCommandUnmodified(command, Logger.debug);
+        public static int runShellCommandUnmodified(@Nonnull String command) {
+            return runShellCommandUnmodified(command, Logger.debug);
         }
 
-        public static void runShellCommandUnmodified(@Nonnull String command, @Nonnull Logger logger) {
-            runShellCommandUnmodified(command, null, logger);
+        public static int runShellCommandUnmodified(@Nonnull String command, @Nonnull Logger logger) {
+            return runShellCommandUnmodified(command, null, logger);
         }
 
-        public static void runShellCommandUnmodified(@Nonnull String command, @Nonnull File directory) {
-            runShellCommandUnmodified(command, directory, Logger.debug);
+        public static int runShellCommandUnmodified(@Nonnull String command, @Nonnull File directory) {
+            return runShellCommandUnmodified(command, directory, Logger.debug);
         }
 
-        public static void runShellCommandUnmodified(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) {
+        public static int runShellCommandUnmodified(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) {
             try {
-                LinuxUtil.runShellCommandUnmodified(command, directory, logger);
+                return LinuxUtil.runShellCommandUnmodified(command, directory, logger);
             } catch (IOException | InterruptedException e) {
                 Logger.error.println(e.getMessage());
+                return -1;
             }
         }
 
-        public static void runShellCommand(@Nonnull String command) {
-            runShellCommand(command, Logger.debug);
+        public static int runShellCommand(@Nonnull String command) {
+            return runShellCommand(command, Logger.debug);
         }
 
-        public static void runShellCommand(@Nonnull String command, @Nonnull Logger logger) {
-            runShellCommand(command, null, logger);
+        public static int runShellCommand(@Nonnull String command, @Nonnull Logger logger) {
+            return runShellCommand(command, null, logger);
         }
 
-        public static void runShellCommand(@Nonnull String command, @Nonnull File directory) {
-            runShellCommand(command, directory, Logger.debug);
+        public static int runShellCommand(@Nonnull String command, @Nonnull File directory) {
+            return runShellCommand(command, directory, Logger.debug);
         }
 
-        public static void runShellCommand(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) {
+        public static int runShellCommand(@Nonnull String command, @Nullable File directory, @Nonnull Logger logger) {
             try {
-                LinuxUtil.runShellCommand(command, directory, logger);
+                return LinuxUtil.runShellCommand(command, directory, logger);
             } catch (IOException | InterruptedException e) {
                 Logger.error.println(e.getMessage());
+                return -1;
             }
         }
 
-        public static void runShellScript(@Nonnull ShellFile script) {
-            runShellScript(script, Logger.debug);
+        public static int runShellScript(@Nonnull ShellFile script) {
+            return runShellScript(script, Logger.debug);
         }
 
-        public static void runShellScript(@Nonnull ShellFile script, @Nonnull Logger logger) {
+        public static int runShellScript(@Nonnull ShellFile script, @Nonnull Logger logger) {
             try {
-                LinuxUtil.runShellScript(script, logger);
+                return LinuxUtil.runShellScript(script, logger);
             } catch (IOException | InterruptedException e) {
                 Logger.error.println(e.getMessage());
+                return -1;
             }
         }
     }
