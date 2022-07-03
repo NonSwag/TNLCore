6c895dff9b76020dc15ecaadb9695888c08d4084
NonSwag <mrminecraft00@gmail.com#>
2022-07-03 13:59:19 +0200

    StringUtil changes

--- src/main/java/net/nonswag/tnl/core/utils/StringUtil.java
+++ src/main/java/net/nonswag/tnl/core/utils/StringUtil.java
@@ -9,20 +9,22 @@ import java.text.DecimalFormat;
 public final class StringUtil {
 
     @Nonnull
-    public static String VALID_CHARS = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+    private static final char[] chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890".toCharArray();
 
     private StringUtil() {
     }
 
     @Nonnull
+    public static String random(@Nonnull char[] chars, int length) {
+        StringBuilder builder = new StringBuilder();
+        if (chars.length == 0) return builder.toString();
+        for (int i = 0; i < length; i++) builder.append(chars[MathUtil.randomInteger(0, chars.length - 1)]);
+        return builder.toString();
+    }
+
+    @Nonnull
     public static String random(int length) {
-        StringBuilder randomString = new StringBuilder();
-        char[] text = new char[length];
-        for (int i = 0; i < length; i++) {
-            text[i] = VALID_CHARS.charAt(MathUtil.randomInteger(0, VALID_CHARS.length() - 1));
-        }
-        for (char c : text) randomString.append(c);
-        return randomString.toString();
+        return random(chars, length);
     }
 
     @Nonnull
